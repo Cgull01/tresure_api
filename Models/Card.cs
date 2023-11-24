63535b26f8ff9002effb6c2f95a5a7a95647668f
Cgull <tomas.motiejunas01@gmail.com>
2023-11-24 14:25:16 +0200

    card dates now default to null

--- Models/Card.cs
+++ Models/Card.cs
@@ -10,10 +10,10 @@ namespace API_tresure.Models
         public string? Title { get; set; }
         public string? Details { get; set; }
         [Column(TypeName = "jsonb")] public string? Tags { get; set; }
-        public DateTime? DueDate { get; set; }
+        public DateTime? DueDate { get; set; } = null;
         public DateTime CreationDate { get; set; }
-        public DateTime? CompletionDate { get; set; }
-        public DateTime? ApprovalDate { get; set; }
+        public DateTime? CompletionDate { get; set; } = null;
+        public DateTime? ApprovalDate { get; set; } = null;
         public List<Member> AssignedMembers { get; set; } = new List<Member>();
         [Required][ForeignKey("Column")] public required int ColumnId { get; set; }
         public Column? Column { get; set; }
